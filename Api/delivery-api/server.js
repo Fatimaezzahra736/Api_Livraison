bfbe09016b8d76d7039a96c08d2c8aa5e58bad7d
Fatimaezzahra736 <elassabifatimazahra@gmail.com>
2025-05-01 22:23:50 +0100

    update

--- Api/delivery-api/server.js
+++ Api/delivery-api/server.js
@@ -1,40 +1,22 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth.routes');
 
+dotenv.config();
 const app = express();
-
-// Middleware JSON
 app.use(express.json());
 
-// Importer routes ici (exemple plus tard)
-// app.use('/api/commandes', require('./routes/commande.routes'));
-// Importer le fichier de route
-const authRoutes = require('./routes/auth.routes');
+// Routes
 app.use('/api/auth', authRoutes);
 
-const clientRoutes = require('./routes/client.routes');
-app.use('/api/clients', clientRoutes);
-
-const commandeRoutes = require('./routes/commande.routes');
-app.use('/api/commandes', commandeRoutes);
-
-const livreurRoutes = require('./routes/livreur.routes');
-app.use('/api/livreurs', livreurRoutes);
-
-
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
 
-// Connexion MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connecté à MongoDB");
-    app.listen(PORT, () => console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`🚀 Serveur lancé sur le port ${PORT}`);
+    });
   })
   .catch(err => console.error("Erreur MongoDB :", err));
-  app.get('/', (req, res) => {
-    res.send('API Running ✅');
-  });
-  
